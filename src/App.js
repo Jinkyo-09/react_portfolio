bdcbeb2dbd74024834e6833e5abb35fea63c4329
jinkyo-09 <rlawlsry0911@gmail.com>
2023-09-26 14:43:32 +0900

    route로 특정 url에 특정 컴포넌트 랩핑

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import Header from './componunts/common/header/header';
 import Department from './componunts/sub/department/department';
+import Youtube from './componunts/sub/youtube/youtube';
 import './styles/Global.scss';
+import { Route } from 'react-router-dom';
 
 function App() {
 	return (
 		<>
 			<Header />
-			<Department />
+			<Route path='/department'>
+				<Department />
+			</Route>
+			<Route path='YouTube'>
+				<Youtube />
+			</Route>
 		</>
 	);
 }
---
+++ src/componunts/sub/youtube/youtube.jsx
@@ -0,0 +1,11 @@
+import Layout from '../../common/layout/layout';
+//import style from './department.module.scss';
+//import clsx from 'clsx';
+
+export default function Youtube() {
+	return (
+		<Layout title={'Department'}>
+			<p>YouTube Page입니다.</p>
+		</Layout>
+	);
+}
